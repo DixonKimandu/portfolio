5d88aecd353b8f0340ed031fb2ff3e45d5fb4160
DixonKimandu <dixonndungu16@gmail.com>
2023-04-07 16:11:06 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "RAIM COLLECTION",
+    subtitle: "Wordpress",
+    description:
+      "Raim Collection is a vintage clothes store that sells a variety of cool products from t-shirts, jackets, caps and shoes",
+    image: "./project-1.gif",
+    link: "https://raimcollection.co.ke",
+  },
+  {
+    title: "MAKTABA STUDIOS",
+    subtitle: "Wordpress",
+    description:
+      "Maktaba Studios is a book store that has a large library with a variety of books",
+    image: "./project-2.gif",
+    link: "https://maktabastudios.co.ke",
+  },
+  {
+    title: "MATICKO",
+    subtitle: "Vuejs",
+    description:
+      "Maticko is an online ticketing platform",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Anvil",
+    subtitle: "HTML, CSS, JavaScript",
+    description:
+      "Anvil is a portfolio site for the anvil organization",
+    image: "./project-4.gif",
+    link: "https://dixonkimandu.github.io",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Creative and Innovative.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Cool Dude",
+    company: "cooldude",
+  },
+  {
+    quote:
+      "Interesting and fun to work with.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Fun dude",
+    company: "fundude",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Vuejs",
+  "Laravel",
+  "Flutter",
+  "PHP",
+];
